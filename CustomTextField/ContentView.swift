1079ddb6db939faf417e6019573dec7f69706baa
Phu Huynh <phu@filesoft.tech>
2020-04-14 23:56:12 +0700

    Complete CustomTextField

--- CustomTextField/ContentView.swift
+++ CustomTextField/ContentView.swift
@@ -9,13 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = "ABCKD"
+    @State var isFirstResponder: Bool = true
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            CustomTextField(text: $text, isFirstResponder: $isFirstResponder)
+                .background(Color.gray)
+                .frame(height: 50)
+            
+            Button("Tap me") {
+                self.isFirstResponder.toggle()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+
+    }
+}
+
+struct CustomTextField: UIViewRepresentable {
+    @Binding var text: String
+    @Binding var isFirstResponder: Bool
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.oneEditingChanged(_:)) ,
+            for: .editingChanged)
+        return textField
+    }
+    
+    func updateUIView(_ textField: UITextField, context: Context) {
+        textField.text = self.text
+        if self.isFirstResponder {
+            textField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    class Coordinator {
+        var customTextField: CustomTextField
+        
+        init(_ customTextField: CustomTextField) {
+            self.customTextField = customTextField
+        }
+        
+        @objc func oneEditingChanged(_ sender: UITextField) {
+            self.customTextField.text = sender.text ?? ""
+        }
     }
 }
